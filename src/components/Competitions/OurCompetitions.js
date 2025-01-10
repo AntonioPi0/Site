5915a166ae6d59157848fa69f04edd44e65672bc
Andrea Falaschi <andrea.falaschi.2005@gmail.com>
2025-01-10 11:55:48 +0100

    Added a Competiions page

---
+++ src/components/Competitions/OurCompetitions.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import '../../css/main.css';
+
+
+const OurCompetitions = () => {
+  return (
+    <section className="competitions-section">
+        <div className="competitions">
+            <h2>Competitions</h2>
+            <p>Our team is committed to <b>continuous growth and learning</b> by participating in a variety of competitions. These events provide invaluable opportunities to <b>enhance our skills</b> and <b>tackle real-world challenges</b>. By engaging in diverse competitions, we aim to broaden our expertise, gain practical experience, and further our passion for innovation and excellence.</p>
+            <h1>Student Design Competition</h1>
+            <p>
+            The <b>Vertical Flight Society</b> annual Student Design Competition engages engineering students worldwide in designing vertical lift aircraft to meet specific requirements,
+            fostering interest in <b>vertical flight technology</b>. Winning teams receive cash stipends and complimentary registration to present their designs at the <b>Society Annual Forum</b>,
+            a premier vertical flight technical event. Sponsored by major industry players like <b>Airbus</b>, <b>Boeing</b>, <b>Bell</b>, and others, the competition promotes education and careers in vertical flight while benefiting from strong industry support.
+            </p>
+            <h1></h1>
+      </div>
+    </section>
+      
+  );
+};
+
+export default OurCompetitions;
